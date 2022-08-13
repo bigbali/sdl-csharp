08494ebc170c053898139d06becf7a0076067d0a
Balázs Burján <bigbali141@gmail.com>
2022-08-13 20:43:15 +0300

    UserControl: ToggleButton

---
+++ sdl-csharp/Resource/Control/ToggleButton.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sdl_csharp.Resource.Control
+{
+    /// <summary>
+    /// Interaction logic for Toggle.xaml
+    /// </summary>
+    public partial class ToggleButton : UserControl
+    {
+        public ToggleButton()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public int ButtonWidth
+        {
+            get { return (int)GetValue(ButtonWidthProperty); }
+            set { SetValue(ButtonWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ButtonWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ButtonWidthProperty = DependencyProperty.Register( // This got removed, but wanna remember the implementation, so let's keep for now
+            "ButtonWidth",
+            typeof(int),
+            typeof(ToggleButton),
+            new PropertyMetadata(40)
+        );
+    }
+}
